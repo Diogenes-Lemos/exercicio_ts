489cb3bbce3ee441205667965cf04d91e5ad0c85
Diogenes <diogenes.lemos1@gmail.com>
2024-09-25 15:53:11 -0300

    Exercicio Corrigido

--- exercicio.ts
+++ exercicio.ts
@@ -1,10 +1,10 @@
 //Parte 1 - Abaixo a função que multiplica dois números  em seu argumento 
 
-function multiplicatorMachine(fator1:number, fator2:number) {
+function multiplicatorMachine(fator1:number, fator2:number):number {
     const f1:number = fator1;
     const f2:number = fator2;
 
-    return(f1*f2)
+    return(f1*f2) //corrigido: Agora está explicito que a função retorna o tipo "number"
 }
 
 console.log(`O resultado da multiplicação é ${multiplicatorMachine(15, 25)}`)
@@ -12,11 +12,11 @@ console.log(`O resultado da multiplicação é ${multiplicatorMachine(15, 25)}`)
 
 //Parte 2 - Abaixo a função que executa a saudação
 
-function oiMachine(nome:string) {
+function oiMachine(nome:string):string {
     const Saudacao:string = 'Olá';
     const Id:string = nome;
 
-    return(Saudacao + Id);
+    return(Saudacao + Id); //corrigido: Agora está explicito que a função retorna o tipo "string"
 }
 
 console.log(oiMachine('Diógenes'))
